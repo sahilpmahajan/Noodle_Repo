af87e46b4be1b4b037346bea51256b15f278b673
Yash Singh <yashdahiya2002@gmail.com>
2021-11-23 23:46:05 +0530

    Added forgot password functionality

--- Noodle/authentication/views.py
+++ Noodle/authentication/views.py
@@ -1,13 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib import messages, auth
-from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import update_session_auth_hash, login
 from django.core.mail.message import EmailMessage
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
 from django.views.generic import CreateView, FormView, RedirectView, UpdateView
-from django.http import Http404
-from django.core.mail import send_mail
+from django.core.mail import send_mail, BadHeaderError
 from django.conf import settings
 from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
@@ -16,6 +14,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.template.loader import render_to_string
+from django.contrib.auth.tokens import default_token_generator
 from .tokens import account_activation_token
 from .forms import *
 from .models import User
@@ -24,10 +23,10 @@ from django.urls import reverse_lazy
 from .decorators import user_is_student, user_is_instructor
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
+from django.db.models.query_utils import Q
 
 #ACTIVATE ACCOUNT
 def activate_account(request, uidb64, token):
@@ -239,6 +238,39 @@ class LogoutView(RedirectView):
         auth.logout(request)
         messages.success(request, 'You are now logged out')
         return super(LogoutView, self).get(request, *args, **kwargs)
+
+#PASSWORD RESET
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			associated_users = get_user_model().objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "authentication/password/password_reset_email.txt"
+					c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Float Moodle',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'yashdahiya2002@gmail.com' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+
+					messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+					return redirect('authentication:password_reset_done')
+			messages.error(request, 'An invalid email has been entered.')
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="authentication/password/password_reset.html", context={"password_reset_form":password_reset_form})
+
 
 #CHANGE STUDENT PASSWORD
 
