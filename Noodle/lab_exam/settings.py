14308a8755fa9a029488d1a0ddf9669ddeee7c12
sahilpmahajan <sahilpmahajan@gmail.com>
2021-11-24 19:11:01 +0530

    Instructor can see only his courses in view-courses afte login

--- Noodle/lab_exam/settings.py
+++ Noodle/lab_exam/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'lab_exam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'noodledb',
+        'USER': 'noodle',
+        'PASSWORD': 'noodle123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
