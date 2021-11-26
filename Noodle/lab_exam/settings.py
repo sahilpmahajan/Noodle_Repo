9b6faccaedae6cfe4037aeca4a2dec0929f2fe14
shubak <shubak134@gmail.com>
2021-11-26 17:04:18 +0530

    Fixed PosixPath error

--- Noodle/lab_exam/settings.py
+++ Noodle/lab_exam/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'lab_exam.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
